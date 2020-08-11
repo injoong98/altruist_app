681ed2cd1decc324a4606e093ee73d2ed618a4b9
geun9716 <crintop@gmail.com>
2020-08-11 23:30:40 +0900

    albaScreen List

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/AlbaScreen.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/AlbaScreen.js
@@ -1,20 +1,94 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Layout,Button,Text} from '@ui-kitten/components'
+import { StyleSheet, View, Image, } from 'react-native';
+import { Card, CardItem, List, Text, Button, Left } from '@ui-kitten/components';
 
+const data = [
+    {
+        id : 1,
+        overLine : "overLine!!",
+        companyName : "(주) 사랑",
+        context : "맥콜은 사랑입니다."
+    },
+    {
+        id : 2,
+        overLine : "overLine2",
+        companyName : "카카오",
+        context : "카카오는 돈이 많습니다 여기로 오세요"
+    },
+    {
+        id : 3,
+        overLine : "overLine3",
+        companyName : "다음",
+        context : "다음은 카카오의 계열사 입니다. 다음와도 카카오에요."
+    }];
 
+export const AlbaScreen = () => {
 
-const AlbaScreen = ({navigation}) =>{
-    
-    return(
-    <SafeAreaView style={{flex:1}}>
-        <Layout style={{flex:1,justifyContent:"center", alignItems:"center"}}>
-            <Text>알바천일국 화면입니다.</Text>
-            <Button onPress = {()=>{navigation.goBack()}}>뒤로가기</Button>
-        </Layout>   
-    </SafeAreaView>
+  // const renderItemHeader = (headerProps, info) => (
+  //   <View {...headerProps}>
+        
+  //   </View>
+  // );
 
-    )
-    }
+  // const renderItemFooter = (footerProps) => (
 
-export {AlbaScreen}
+  // );
+
+  const renderItem = (info) => (
+    <Card
+      style={styles.item}
+      status='basic'>
+        <View>
+        <View style={{flexDirection : 'row'}}>
+            <View style={styles.Text}>
+                <Text>{info.item.overLine}</Text>
+                <Text style={{fontSize : 20}}>{info.item.companyName}</Text>
+                <Text style={{marginTop : 10}}>{info.item.context}</Text>
+            </View>
+            <View style={styles.image}>
+                <Image source={require('../../../assets/social_kakao.png')}/>
+            </View>
+        </View>
+        
+        <Button style={styles.button} appearance='ghost' onPress={()=>{
+                alert('detail screen');
+            }}>상세정보</Button>
+        </View>
+    </Card>
+  );
+
+  return (
+    <List
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={data}
+      renderItem={renderItem}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  contentContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  item: {
+    marginVertical: 4,
+  },
+  Text : {
+    flex : 2,
+    marginLeft : 20,
+    marginTop : 10,
+    },
+image :{
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+},
+  button:{
+    width : 100,
+  }
+});
