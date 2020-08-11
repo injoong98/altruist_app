30b352a7e474a104e60dfc81db225cc021394770
Tune <cfdw011@gmail.com>
2020-08-11 20:38:38 +0900

    #559 글본문,글작성 스크린 오류 수정 했습니다.

--- react_native/AltruistApp/L1Stack/Content.js
+++ react_native/AltruistApp/L1Stack/Content.js
@@ -2,21 +2,22 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {Layout,Button,Text,TopNavigation,TopNavigationAction,Icon} from '@ui-kitten/components'
 
-const BackIcon =  () =>(
-    <Icon name = "arrow-back"/>
+const BackIcon =  (props) =>(
+    <Icon {...props} name = "arrow-back"/>
 )
 
 
 const defaultContent = ({navigation}) =>{
     
     const BackAction = () =>(
-        <TopNavigationAction icon={BackIcon} onPress={navigation.goBack()}/>
+        <TopNavigationAction icon={BackIcon} onPress={() =>{navigation.goBack()}}/>
     )
     
     return(
     <SafeAreaView style={{flex:1}}>
+        <TopNavigation title="글작성" alignment="center" accessoryLeft={BackAction} /> 
         <Layout style={{flex:1,justifyContent:"center", alignItems:"center"}}>
-     <Text>글본문입니다</Text>
+            <Text>글본문입니다</Text>
         </Layout>   
     </SafeAreaView>
 
