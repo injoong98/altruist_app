a99d6e91cd6f17684aea9520994b4befe210f454
Tune <cfdw011@gmail.com>
2020-08-12 12:12:04 +0900

    #568 고민글리스트

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/GominScreen.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/GominScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {SafeAreaView,View,StyleSheet} from 'react-native';
-import {Layout,Button,Text,ListItem,List, Divider,Card} from '@ui-kitten/components'
+import { Icon,Layout,Button,Text,ListItem,List, Divider,Card} from '@ui-kitten/components'
 
 
 const sample = [
     {
-        title:"first title",
+        title:"first title 고민있어요 제목을 길게 써보면 어떻게 나올까요",
         author:"Tune",
         view:"3023",
         up:"200",
@@ -86,10 +86,33 @@ const sample = [
 
 const GominScreen = ({navigation}) =>{
     
-      const renderItem = ({ item, index }) => (
+    const EyeIcon = (props)=>(
+        <Icon style={{width: 24,height: 24,marginLeft:5}} fill='#8F9BB3' name="eye"/>
+    )
+    const CommentIcon = (props)=>(
+        <Icon style={{width: 24,height: 24,marginLeft:5}} fill='#8F9BB3' name="message-circle"/>
+    )
+    const HeartIcon = (props)=>(
+        <Icon style={{width: 24,height: 24,marginLeft:5}} fill='#8F9BB3' name="heart"/>
+    )
+        
+    const renderItem = ({ item, index }) => (
         <Card>
-            
+            <Text category="h6" numberOfLines={1} ellipsizeMode="tail">{item.title}</Text>
+            <View style={{marginTop:10, display:"flex",flexDirection:"row", justifyContent:"space-between"}}>
+                <Text category="s1">{item.author}</Text>
+                <View style={{display:"flex",flexDirection:"row"}}>
+                    <EyeIcon />
+                    <Text category="s1">{item.view}</Text>
+                    <HeartIcon />
+                    <Text category="s1">{item.up}</Text>
+                    <CommentIcon />
+                    <Text category="s1">{item.comment}</Text>
+                </View>
+
+            </View>
         </Card>
+
       );
 
         
@@ -100,7 +123,7 @@ const GominScreen = ({navigation}) =>{
             data ={sample}
             ItemSeparatorComponent={Divider}
             renderItem={renderItem} />
-            <View style ={{ width:"100%",bottom:30,position :"absolute",display :"flex", flexDirection:"row",justifyContent:"center", alignItems:"center"}}>
+            <View style ={styles.buttoncontainer}>
                 <Button onPress={()=>{navigation.navigate('Write')}} >글쓰기</Button>
             </View>
         </SafeAreaView>
@@ -110,3 +133,13 @@ const GominScreen = ({navigation}) =>{
     }
 
 export {GominScreen}
+
+const styles = StyleSheet.create({
+    buttoncontainer:{
+        width:"100%",bottom:30,position :"absolute",
+        display :"flex", 
+        flexDirection:"row",
+        justifyContent:"center", 
+        alignItems:"center"
+    }
+})
