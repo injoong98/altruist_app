18788dac3bc710233a62d856b4f0514877af28d0
geun9716 <crintop@gmail.com>
2020-08-21 21:02:33 +0900

    이타주의자 profile list form

--- react_native/AltruistApp/L1Stack/L2Bottom/ComBottomNav.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/ComBottomNav.js
@@ -40,38 +40,6 @@ const FontScreen =() =>(
 const SpareScreen =({navigation}) =>(
     <SafeAreaView style={{flex:1, backgroundColor : '#E4E4E4'}}>
         <TopNavigation title="스페어" alignment="center"/> 
-        <Card style = {{backgroundColor:'white', borderRadius : 20, margin : 10}}>
-            <View style = {{flexDirection : 'row', justifyContent:'flex-end'}}>
-                <Text style = {{backgroundColor : '#b9b5d6', borderRadius : 20, padding : 4, marginHorizontal : 5}}>IT개발</Text>
-                <Text style = {{backgroundColor : '#A7D4DE', borderRadius : 20, padding : 4, marginHorizontal : 5}}>스타트업/창업</Text>
-                <Text style = {{backgroundColor : '#eab0b3', borderRadius : 20, padding : 4, marginHorizontal : 5}}>UX/UI기획</Text>
-            </View>
-            <View style={{flexDirection : 'row', alignItems : 'flex-end', justifyContent : 'flex-start'}}>
-                <Image source={require('../../assets/social_kakao.png')} style ={{width : 100, height : 100, resizeMode:'contain'}}/>
-                <View style={{marginLeft : 10}}>
-                    <Text category = 'h1'>홍길동</Text>
-                    <Text category = 'h6'>저는 주부 9단 입니다.</Text>
-                </View>
-            </View>
-            <View style={{flexDirection : 'row'}}>
-                <Icon style={{width : 40, height : 40}} fill='yellow' name='star'/>
-                <Icon style={{width : 40, height : 40}} fill='yellow' name='star'/>
-                <Icon style={{width : 40, height : 40}} fill='yellow' name='star'/>
-                <Icon style={{width : 40, height : 40}} fill='yellow' name='star'/>
-                <Icon style={{width : 40, height : 40}} fill='yellow' name='star'/>
-            </View>
-            <Text category='h6' style={{fontSize : 16}}>학력 또는 직장을 입력합니다.</Text>
-            <View style = {{flexDirection : 'row'}}>
-                <View style = {{flexDirection : 'row', flex : 5}}>
-                    <Text style = {{backgroundColor : '#eab0b3', borderRadius : 20, padding : 4, marginHorizontal : 5, fontSize : 16, textAlignVertical : 'center'}}>IT개발</Text>
-                    <Text style = {{backgroundColor : '#eab0b3', borderRadius : 20, padding : 4, marginHorizontal : 5, fontSize : 16, textAlignVertical : 'center'}}>스타트업/창업</Text>
-                    <Text style = {{backgroundColor : '#eab0b3', borderRadius : 20, padding : 4, marginHorizontal : 5, fontSize : 16, textAlignVertical : 'center'}}>UX/UI기획</Text>
-                </View>
-                <Button 
-                    style = {{height : 20}}
-                    onPress={()=>{alert('question')}}>지원하기</Button>
-            </View>
-        </Card>
         <Layout style={{flex:1,justifyContent:"center", alignItems:"center"}}>
             <Text>스페어</Text>
             <Button onPress={()=>{navigation.goBack()}}>뒤로가기</Button>
