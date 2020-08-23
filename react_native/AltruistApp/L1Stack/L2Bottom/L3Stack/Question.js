7a576428b9d7e31351bf8330b811584a528436c1
Tune <cfdw011@gmail.com>
2020-08-23 11:06:16 +0900

    #628 질문 작성 form 초안

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/Question.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/Question.js
@@ -1,26 +1,72 @@
 import React from 'react';
 
-import {SafeAreaView} from 'react-native'
-import {Layout,Text,TopNavigation,Button,Icon, TopNavigationAction} from '@ui-kitten/components'
+import {SafeAreaView,TextInput,View,StyleSheet} from 'react-native'
+import {Layout,Text,TopNavigation, Button,Icon, TopNavigationAction} from '@ui-kitten/components'
 
 const BackIcon =  (props) =>(
     <Icon {...props} name = "arrow-back"/>
 )
 
-const AltQuestionScreen =({navigation}) =>{
+class AltQuestionScreen extends React.Component
+{
+    constructor(props){
+        super(props);
+        this.state={
+            title:'',
+            content:'',
+            userid_send:'',
+            user_recieve:'',
+        }
+    }
 
-    const BackAction = () =>(
-        <TopNavigationAction icon={BackIcon} onPress={() => {navigation.goBack()}}/>
-        )
- 
-    return(
-    <SafeAreaView style={{flex:1}}>
-        <TopNavigation title="이타주의자" alignment="center" accessoryLeft={BackAction}/> 
-        <Layout style={{flex:1,justifyContent:"center", alignItems:"center"}}>
-            <Text>이타주의자 질문</Text>
-        </Layout>   
-    </SafeAreaView>
+    BackAction = () =>(
+        <TopNavigationAction icon={BackIcon} onPress={() => {this.props.navigation.goBack()}}/>
     )
+
+    render(){
+        const {title,content} = this.state;
+
+        return(
+        <SafeAreaView style={{flex:1}}>
+            <TopNavigation title="이타주의자" alignment="center" accessoryLeft={this.BackAction}/> 
+            <View style={{ flex:1,backgroundColor:"#f4f4f4",padding:10}}>
+                <View>
+                    <Text>이타주의자에게 질문</Text>
+                </View>
+               <View>
+                    <Text style={styles.fieldtitle}>제목</Text>
+                    <TextInput style={styles.titleInput} value={title} onChangeText={text =>this.setState({title:text})}/>
+               </View>
+               <View>
+                    <Text style={styles.fieldtitle}>내용</Text>
+                    <TextInput style={styles.contentInput} value={content} onChangeText={text =>this.setState({content:text})}/>
+               </View>
+            </View>
+            <Button>질문 보내기</Button>
+        </SafeAreaView>
+        )
+
+    }
 }
+
+const styles = StyleSheet.create({
+    titleInput :{
+        backgroundColor:'#ffffff',
+        borderRadius:15,
+        marginHorizontal:10,
+        marginBottom:20
+    },
+    contentInput :{
+        backgroundColor:'#ffffff',
+        borderRadius:15,
+        marginHorizontal:10,
+        marginBottom:20,
+        height:200
+    },
+    fieldtitle:{
+        padding :10
+    }
+
+})
 
 export default AltQuestionScreen;
