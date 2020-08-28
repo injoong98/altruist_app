6f62c24cd175df22d592ad7363dac8011d6bdf6f
Tune <cfdw011@gmail.com>
2020-08-28 19:25:39 +0900

    #612 고민있어요 게시판 글리스트 디자인 적용

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/GominScreen.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/GominScreen.js
@@ -62,18 +62,24 @@ class GominScreen extends React.Component {
                 <Text style={styles.subtext}category="s2" numberOfLines={1}>{post_remove_tags}</Text>
             </View>
             <View style={styles.subtitle}>
-                <View style={{display:'flex',flexDirection:'row',alignItems:'flex-end',padding:10}}> 
-                    <Text category="s2">{item.display_name}</Text>
-                    {/* <Text category="s2">{item.post_datetime}</Text> */}
+                <View style={{display:'flex',flexDirection:'row',alignItems:'flex-end',marginBottom:4}}> 
+                    <Text category="s2" style={{fontWeight:'bold',marginRight:5}}>{item.display_name}</Text>
                     <PostTime datetime = {item.post_datetime}/>
                 </View>
                 <View style={styles.infocontainer}>
-                    <HeartIcon />
-                    <Text style={styles.infotext} category="s1">{item.post_like}</Text>
-                    <CommentIcon />
-                    <Text style={styles.infotext} category="s1">{item.post_comment_count}</Text>
-                    <EyeIcon />
-                    <Text style={styles.infotext} category="s1">{item.post_hit}</Text>
+                    <View style={{alignItems:'center'}}>
+                        <HeartIcon />
+                        <Text style={styles.infotext} category="s1">{item.post_like}</Text>
+                    </View>
+                    <View style={{alignItems:'center'}}>
+                        <CommentIcon />
+                        <Text style={styles.infotext} category="s1">{item.post_comment_count}</Text>
+                    </View>
+                    <View style={{alignItems:'center'}}>
+                        <EyeIcon />
+                        <Text style={styles.infotext} category="s1">{item.post_hit}</Text>
+                    </View>
+
                 </View>
 
             </View>
@@ -148,22 +154,22 @@ class GominScreen extends React.Component {
                 <Spinner size="giant" />
             </Text>
         </View>:
-        <SafeAreaView style={{flex:1}}>
+        <SafeAreaView style={{flex:1,backgroundColor:"#ffffff"}}>
             {/* <Animated.View style={{overflow:'hidden',height:this.state.searchInput,borderStyle:'solid',borderColor:'red',borderWidth:1}}>
                 <Text>
                     안녕하세요
                 </Text>
             </Animated.View> */}
             <List
-            data ={this.state.lists}
-            ItemSeparatorComponent={Divider}
-            renderItem={this.renderItem} 
-            onRefresh={this.onRefresh}
-            refreshing={this.state.refreshing}
-            onEndReached={this.load_more_data}
-            onEndReachedThreshold = {0.9}
-            ListFooterComponent={this.renderFooter}
-            style={{backgroundColor:'#ffffff'}}/>
+                data ={this.state.lists}
+                renderItem={this.renderItem} 
+                onRefresh={this.onRefresh}
+                refreshing={this.state.refreshing}
+                onEndReached={this.load_more_data}
+                onEndReachedThreshold = {0.9}
+                ListFooterComponent={this.renderFooter}
+                style={{backgroundColor:'#ffffff'}}
+            />
             <View style ={styles.buttoncontainer}>
                 <Button style={{width:"100%"}} onPress={()=>{this.props.navigation.navigate('GominWrite',{statefunction:this.statefunction})}} >
                     글쓰기
@@ -182,11 +188,12 @@ export {GominScreen}
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:"#E3E3E3",
+        backgroundColor:"#F4F4F4",
         borderRadius : 20,
-        marginVertical:5,
-        marginHorizontal:15,
+        marginVertical:4.5,
+        marginHorizontal:19,
         padding:0,
+        paddingLeft:21
 
 
     },
@@ -197,32 +204,36 @@ const styles = StyleSheet.create({
         alignItems:"center"
     },
     icon:{
-        width: 24,height: 24,marginLeft:5
+        width: 15,height: 15
     },
     subtitle:{
         marginTop:10, display:"flex",flexDirection:"row", justifyContent:"space-between",
     },
     infocontainer:{
-        display:"flex",flexDirection:"row",
-        borderTopLeftRadius:20,
+        display:"flex",flexDirection:"row",justifyContent:'space-evenly',
+        borderTopLeftRadius:23,
+        width:116,
         backgroundColor:"#ffffff",
         position:"relative",bottom:0,right:0,
-        padding:5
+        paddingTop:5,
+        paddingLeft:20,
+        paddingRight:10
     },
     loader:{
         marginTop : 10,
         alignItems : 'center',
     },
     infotext:{
-        color:'#141552'
+        color:'#141552',
+        fontSize:9
     },
     headtext:{
+        marginTop:11,
         paddingTop:10,
-        paddingLeft:20,
         fontWeight:'bold'
     },
     subtext:{
-        marginLeft:20,
+        marginTop:5,
         maxWidth:200
     }
 })
