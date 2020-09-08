d8b5d7d6ce2b0fb2b3b60e1e57689f6598cfcc75
Tune <cfdw011@gmail.com>
2020-09-08 16:04:20 +0900

    #701 마이페이지 메인 프론트 엔드

--- react_native/AltruistApp/L1Stack/L2Bottom/Mypage.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/Mypage.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {Text,View,StyleSheet} from 'react-native';
-import {Input,Button} from '@ui-kitten/components';
+import {View,StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native';
+import {Input,Button,Text} from '@ui-kitten/components';
 import axios from 'axios'
 import AsyncStorage from '@react-native-community/async-storage';
+
+import MoreSvg from '../../assets/icons/dotdotdot-large.svg'
+import ThumbSvg from '../../assets/icons/thumb-up-filled.svg'
+import PencilSvg from '../../assets/icons/pencil-outline.svg'
+import MessageSvg from '../../assets/icons/message.svg'
 
 class Mypage extends React.Component{
     constructor(props){
@@ -72,13 +77,101 @@ class Mypage extends React.Component{
     }
     render(){
         return(
-            <View style={{flex:1,justifyContent:"center",alignItems:'center'}}>
-                <Button onPress={()=>{this.doLogout();this.removeValue();this.props.navigation.navigate('Login')}}>간단로그아웃</Button>
-                <Button onPress={()=>{this.sessionChk();}}>session chk</Button>
-                <Button onPress={()=>{this.getData('logininfo');this.getData('autologin')}} >AsyncStorage check</Button>
-            </View>
+          <SafeAreaView>
+              <View style={{flexDirection:'row',margin:35,borderWidth:1}}>
+                <View style={{height:95,width:95,borderWidth:1,marginVertical:20,marginLeft:30}}>
+                  <Text>이미지</Text>
+                </View>
+                <View style={{borderWidth:1,marginLeft:16,marginTop:13,marginBottom:24,justifyContent:'space-between'}}>
+                  <View style={{borderWidth:1,padding:8,flexDirection:'row'}}>
+                    <View style={{}}>
+                      <Text category='p2'style={{color:'#A897C2',borderWidth:1,fontSize:10,textAlignVertical:'bottom'}}>point{`\n`}score</Text>
+                    </View>
+                    <View style={{borderWidth:1, marginVertical:3,marginLeft:4,borderColor:'#F0F0F0'}}>
+                    </View>
+                    <View style={{marginLeft:10}}>
+                      <Text style={{fontSize:10,lineHeight:13,color:'#63579D'}}>누적 000{`\n`}사용 000</Text>
+                    </View>
+                  </View>  
+                  <View style={{borderWidth:1,marginTop:15,display:'flex',flexDirection:'row', alignItems:'flex-end'}}>
+                   <Text category='h2' style={{fontSize:24,color:'#63579D'}}>하하하</Text>
+                   <View style={{height:'80%',marginLeft:10,alignItems:'flex-end'}}>
+                    <ThumbSvg height={24} width={24}/>          
+                   </View>
+                  </View>  
+                  <View style={{marginTop:10}}>
+                   <Text style={{fontSize:9}}>자기소개</Text>          
+                  </View>  
+                </View>
+                <TouchableOpacity onPress={()=>{alert('more')}} style={{position:'absolute',borderWidth:1 }}>
+                  <MoreSvg height={19} width={19}/>
+                </TouchableOpacity > 
+              </View>
+              <View style={{borderWidth:1, marginHorizontal:40}}>
+                <View style={{flexDirection:'row',alignItems:'center',marginBottom:23}}>
+                  <PencilSvg height={28} width={22}/>
+                  <Text category='h2' style={styles.menuTitle}>나의 활동</Text>
+                </View>
+                <View>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}} >
+                    <Text style={styles.menuItem}>작성글</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}}>
+                    <Text style={styles.menuItem}>댓글단 글</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}}>
+                    <Text style={styles.menuItem}>작성 댓글</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}}>
+                    <Text style={styles.menuItem}>좋아요한 글</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}}>
+                    <Text style={styles.menuItem}>스크랩</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={{borderWidth:1, marginHorizontal:40}}>
+                <View style={{flexDirection:'row',alignItems:'center',marginBottom:23}}>
+                  <View style={{borderWidth:1,transform: [{ rotate: "25.69deg" }]}}>
+                    <MessageSvg height={28} width={22}/>
+                  </View>
+                  <Text category='h2' style={styles.menuTitle}>쪽지함</Text>
+                </View>
+                <View>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}} >
+                    <Text style={styles.menuItem}>받은  쪽지함</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.menuContainer} onPress={()=>{alert('menu!')}}>
+                    <Text style={styles.menuItem}>보낸 쪽지함</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={{flex:1,display:'flex',flexDirection:'row'}}>
+                  <Button onPress={()=>{this.doLogout();this.removeValue();this.props.navigation.navigate('Login')}}>간단로그아웃</Button>
+                  <Button onPress={()=>{this.sessionChk();}}>session chk</Button>
+                  <Button onPress={()=>{this.getData('logininfo');this.getData('autologin')}} >AsyncStorage check</Button>
+              </View>
+          </SafeAreaView>
         )
     }
 }
 
 export default Mypage;
+
+const styles = StyleSheet.create({
+  menuTitle :{
+    fontSize:12,
+    lineHeight:13,
+    color:'#63579D'
+  },
+  menuContainer:{
+    paddingLeft:14
+  },
+  menuItem:{
+    fontSize:12,
+    lineHeight:13,
+    color:'#63579D',
+    marginBottom:11
+  }
+
+})
