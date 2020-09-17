160a5ae9816a374f0470c6395b07156ab1c732fd
Tune <cfdw011@gmail.com>
2020-09-17 14:02:01 +0900

    #753 안드로이드 FCM console 푸쉬 알림 처리

--- react_native/AltruistApp/App.js
+++ react_native/AltruistApp/App.js
@@ -7,8 +7,8 @@
  */
 
 
-import React from "react";
-import {StatusBar} from "react-native";
+import React,{useEffect} from "react";
+import {StatusBar,Alert} from "react-native";
 import * as eva from '@eva-design/eva';
 import { NavigationContainer } from "@react-navigation/native";
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
@@ -17,9 +17,22 @@ import {StackNav} from "./L1Stack/StackNav";
 import { AppIconsPack } from './app-icons-pack';
 import { default as customtheme } from './custom-theme.json';
 import { default as mapping } from './mapping.json';
-import {AltIconsPack} from './alt-icons'
+import {AltIconsPack} from './alt-icons';
+import messaging from '@react-native-firebase/messaging';
 const App = () => {
-  StatusBar.setBackgroundColor('#B09BDE')
+  StatusBar.setBackgroundColor('#B09BDE');
+  useEffect(() => {
+    messaging()
+    .subscribeToTopic('weather')
+    .then(() => console.log('Subscribed to topic!'));
+    
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
+
   return (
   <>
     <IconRegistry icons={[EvaIconsPack,AltIconsPack]} />
