76154c91d68203df18eabfd56c9058a273083cbe
injoong98 <68066289+injoong98@users.noreply.github.com>
2020-09-22 22:11:37 +0900

    #826 수수마켓 리스트 디자인 적용

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/MarketScreen.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Toptab/MarketScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {Image, ActivityIndicator, SafeAreaView, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction, Button, List, Card, Spinner} from '@ui-kitten/components';
 import axios from 'axios'
+import {PostTime} from '../../../components/PostTime'
 
 import Writesvg from '../../../assets/icons/write.svg'
 
@@ -64,33 +65,53 @@ class MarketScreen extends React.Component {
   renderItem = ({item}) => (
     <TouchableOpacity style={styles.item} onPress={() => {this.props.navigation.navigate('MarketContent',{OnGoback:() =>this.onRefresh(), post_id:item.post_id})}}>
         <View style={styles.textArea}>
-          <View style={styles.textTop}>
-            <Text style={styles.text} category='h4'>
-                {item.title}
-            </Text>
-          </View>
-          <View style={{}}>
-            <Text style={{marginLeft:4, color : 'gray'}} category='c2'>
-                {item.post_location} 
-            </Text>
-          </View>
-          <View style={styles.textBottom}>
-            <View style={{flex:1, justifyContent:'center'}}>
-                <Text style={styles.text} category='h5'>
-                    {item.deal_price.replace(/(<([^>]+)>)/ig,"")} 원
-                </Text>
+          <View style={{flexDirection:'row'}}>
+            {item.deal_status==0?
+            <View style={{flex:1}}>
+              <Text style={{marginLeft:-4, color : '#D4787D', fontSize:11}} category='c2'>
+                •판매완료
+              </Text>
             </View>
-            <View style={{justifyContent: 'center'}}>
-                <Text style={styles.text}>
+            :
+            <View style={{flex:1}}>
+              <Text style={{marginLeft:-4, color : '#439DB1', fontSize:11}} category='c2'>
+                {item.deal_type==0? '•판매중 / 배송':item.deal_type==1? '•판매중 / 직거래':'•판매중 / 배송 & 직거래'}
+              </Text>
+            </View>
+            }
+            <View style={{flexDirection: 'row-reverse', marginTop:3}}>
+                <Text category='h4' style={{fontSize:11}}>
                     {item.post_nickname}
                 </Text>
+                <View style={{position:'absolute', top:20}}>
+                  <PostTime datetime={item.post_datetime}/>
+                </View>
+            </View>
+          </View>
+          <View style={{flex:1, backgroundColor:'white', paddingHorizontal:5, paddingVertical:3, borderRadius:15, marginTop:5, justifyContent:'space-between', marginRight:30}}>
+            <View style={styles.textTop}>
+              <Text style={styles.text} category='h4'>
+                  {item.title}
+              </Text>
+            </View>
+            <View style={styles.textBottom}>
+              <View style={{flex:1, justifyContent:'center'}}>
+                  <Text style={{...styles.text, color:'#878787', fontSize:11}} category='s1'>
+                    {item.post_location}
+                  </Text>
+              </View>
+              <View style={{flex:1, alignItems:'flex-end'}}>
+                  <Text style={{...styles.text, color:'#878787', fontSize:11}} category='s1'>
+                    {item.deal_price.replace(/(<([^>]+)>)/ig,"")} 원
+                  </Text>
+              </View>
             </View>
           </View>
         </View>
         <View style={{width:100, justifyContent:'center', alignItems:'center'}}>
             <Image 
               source={item.origin_image_url? {uri : 'http://dev.unyict.org'+item.origin_image_url}:{uri : 'http://dev.unyict.org'+item.thumb_url}} 
-              style={{width:86, height:86, resizeMode:'cover', borderRadius:10}}
+              style={{width:86, height:86, resizeMode:'cover', borderRadius:15}}
             />
         </View>
     </TouchableOpacity>
@@ -160,7 +181,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white'
     },
     contentContainer: {
-        paddingHorizontal: 8,
+        paddingHorizontal: 10,
         paddingVertical: 4,
     },
     item: {
@@ -169,17 +190,17 @@ const styles = StyleSheet.create({
         height:100, 
         margin:5,
         backgroundColor:'#F4F4F4',
-        borderRadius:10,
+        borderRadius:15,
     },
     textArea: {
         flex: 1,
         paddingHorizontal: 10,
-        paddingVertical: 5,
+        paddingVertical: 10,
+        paddingLeft: 10,
         maxHeight: 100,
     },
     textTop: {
-        flex: 1,
-        // justifyContent: 'center'
+        justifyContent: 'center'
     },
     textBottom: {
         flexDirection: 'row'
