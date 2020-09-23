5a4020f8838dd73889580559776f938224303f70
Tune <cfdw011@gmail.com>
2020-09-24 00:06:02 +0900

    #806 지원하기 form 파일 첨부 기능

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/ApplyForm.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/ApplyForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import DocumentPicker from 'react-native-document-picker';
 import {
   SafeAreaView,
   View,
@@ -8,6 +9,7 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   Alert,
+  TouchableHighlight
 } from 'react-native';
 import {
   Layout,
@@ -19,6 +21,8 @@ import {
   Radio,
   CheckBox,
   Card,
+  Select,
+  SelectItem
 } from '@ui-kitten/components';
 import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import {Signing} from '../../Context';
@@ -36,17 +40,140 @@ class AltApplyFormScreen extends React.Component {
       alt_answertype: '',
       alt_status: '',
       alt_honor: '',
-      acv_type: '',
-      acv_year: '',
-      acv_content: '',
-      acv_status: '',
-      acv_open: '',
-      act_id: '',
+      acv_type: [],
+      acv_year: [],
+      acv_content: [],
+      acv_status: [],
+      acv_open: [],
+      act_id: [],
+      acv_final:[],
+      acv_file1:[],
+      count:1,
+      arrayForLoop:[]
     };
   }
   static contextType = Signing;
+  cancleCareer= (i) =>{
+    const {arrayForLoop,count,acv_open,acv_type,acv_year,acv_content,acv_final,acv_file1}=this.state;
+    console.log(count);
+
+    arrayForLoop.splice(i,1);
+    acv_open.splice(i,1);
+    acv_type.splice(i,1);
+    acv_year.splice(i,1);
+    acv_content.splice(i,1);
+    acv_final.splice(i,1);
+    acv_file1.splice(i,1);
+    this.setState({count:count-1,arrayForLoop,acv_open,acv_type,acv_year,acv_content,acv_final,acv_file1})
+    console.log(count)
+  }
+
+  arrayForLoop = () => {
+    const {acv_open,acv_type,acv_year,acv_content,acv_final,acv_file1}=this.state;
+
+    this.setState({count:this.state.count+1});
+    acv_open[this.state.count-1]=false;
+    acv_type[this.state.count-1]='';
+    acv_year[this.state.count-1]='';
+    acv_content[this.state.count-1]='';
+    acv_final[this.state.count-1]=false;
+    acv_file1[this.state.count-1]=false;
+    var arrayForLoop = []
+    for(var i=0 ;i<this.state.count;i++){
+      arrayForLoop[i]=''
+    }
+    this.setState({arrayForLoop})
+  }
+
+  RenderCareerInput = (props)=>{
+    const [selectedIndex,setSelectedIndex] = React.useState();
+    const {i} = props
+    const {acv_open,acv_type,acv_year,acv_content,acv_final} = this.state;
+    return(
+          <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
+                      <View>
+                            <CheckBox
+                              checked={acv_open[i]}
+                              onChange={nextChk => {acv_open[i]=nextChk;this.setState({acv_open});console.log(acv_open)}}
+                            />
+                      </View>
+                      <View style={{width:100}}>
+                      <Select
+                          value={ ()=>
+                            <Text>
+                              {!acv_type[i]? '선택': acv_type[i] == 'H'? '학력':acv_type[i] == 'J'? '직장' : '기타'}
+                            </Text>
+                          }
+                          selectedIndex={selectedIndex}
+                          onSelect={index =>{
+                            setSelectedIndex(index);
+                            acv_type[i]=index.row==0? 'H':index.row==1? 'J':'E';
+                            this.setState({selectedIndex,acv_type});
+                          }}
+                          >
+                          <SelectItem title='학력'/>
+                          <SelectItem title='직장'/>
+                          <SelectItem title='기타'/>
+                        </Select>
+                      </View>
+                      <View>
+                          <TextInput
+                            value= {acv_year[i]}
+                            onChangeText={(text)=>{acv_year[i]=text;this.setState({acv_year});}}
+                            placeholder='ex)2010~'
+                            style={{backgroundColor:'#ffffff'}}
+                            keyboardType='default'
+                          />
+                      </View>
+                      <View>
+                          <TextInput
+                            value= {acv_content[i]}
+                            onChangeText={(text)=>{acv_content[i]=text;this.setState({acv_content})}}
+                            placeholder='경력내용'
+                            style={{backgroundColor:'#ffffff'}}
+                          />
+                      </View>
+                      <View>
+                      <CheckBox
+                              checked={acv_final[i]}
+                              onChange={nextChk => {acv_final[i]=nextChk;this.setState({acv_final})}}
+                            />
+                      </View>
+                      <TouchableHighlight onPress={()=>this.cancleCareer(i)}>
+                        <Text>x</Text>
+                      </TouchableHighlight>
+            </View>
+    )
+  }
   
-  handleSubmit() {
+  attatchFile= async (i)=>{
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+      console.log(
+        'uri'+res.uri,
+        'type'+res.type, // mime type
+        'name'+res.name,
+        'size'+res.size
+      );
+      const file = {
+        uri:res.uri,
+        type:res.type, // mime type
+        name:res.name,
+        size:res.size
+      }
+      this.state.	acv_file1[i]=file
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
+    }
+    
+  }
+  handleSubmit(){
     const {userForm, user} = this.props;
 
     if (userForm.valid) {
@@ -170,16 +297,12 @@ class AltApplyFormScreen extends React.Component {
   componentDidMount() {
     //this.setAltruist()
     this.getAreaCat();
-    this.setState({mem_id:this.context.session_mem_id})
+    this.setState({mem_id:this.context.session_mem_id});
+    this.arrayForLoop();
   }
 
-  // renderItem = ({item, key}) => {
-  //     console.log(item);
-  // }
-
   render() {
-    const {category} = this.state;
-    console.log('cat : ' + this.props);
+    const {arrayForLoop,acv_open,acv_type,acv_file1,acv_year,acv_content,acv_final} = this.state;
     return (
       <SafeAreaView style={styles.container}>
         <TopNavigation title="이타주의자" accessoryLeft={this.BackAction} />
@@ -227,49 +350,53 @@ class AltApplyFormScreen extends React.Component {
           </View>
 
           {/* 경력구분 */}
-          <View style={{flexDirection: 'row'}}>
-            <Text>경력년도</Text>
-            <TextInput
-              style={styles.contentInput}
-              value={this.state.acv_type}
-              onChangeText={(text) => this.setState({careertype: text})}
-            />
+          <Text>경력 사항</Text>
+          <View>
+            <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
+                <View>
+                  <Text style={{fontSize:12,lineHeight:16}}>
+                  공개
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontSize:12,lineHeight:16}}>
+                  구분
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontSize:12,lineHeight:16}}>
+                  년도
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontSize:12,lineHeight:16}}>
+                  내용
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontSize:12,lineHeight:16}}>
+                  최종 경력
+                  </Text>
+                </View>
+            </View>
+            {
+              arrayForLoop.map((item,index)=>(
+                <this.RenderCareerInput i={index}/>
+              ))
+            }
+            <View style={{flexDirection:'row'}}>
+              <Button onPress={()=>{this.arrayForLoop();}}>+</Button>
+            </View>
           </View>
-          <View style={{flexDirection: 'row'}}>
-            <Text>경력내용</Text>
-            <TextInput
-              style={styles.contentInput}
-              value={this.state.acv_year}
-              onChangeText={(text) => this.setState({careeryear: text})}
-            />
-          </View>
-          <View style={{flexDirection: 'row'}}>
-            <Text>최종경력여부</Text>
-            <TextInput
-              style={styles.contentInput}
-              value={this.state.acv_content}
-              onChangeText={(text) => this.setState({careerfinal: text})}
-            />
-          </View>
-          {/* <View style={{flexDirection:'row'}}>
-                            <Text>증빙용첨부파일</Text>
-                            <TextInput style={styles.contentInput} value={this.state.acv_content} onChangeText={text =>this.setState({careerfile:text})}/>
-                        </View> */}
-          <View style={{flexDirection: 'row'}}>
-            <Text>경력인증상태</Text>
-            <TextInput
-              style={styles.contentInput}
-              value={this.state.acv_status}
-              onChangeText={(text) => this.setState({careerstatus: text})}
-            />
-          </View>
-          <View style={{flexDirection: 'row'}}>
-            <Text>공개여부</Text>
-            <TextInput
-              style={styles.contentInput}
-              value={this.state.acv_open}
-              onChangeText={(text) => this.setState({careeropen: text})}
-            />
+          <Text>경력 증빙 자료</Text>
+          <View>
+          {
+            arrayForLoop.map((item,index)=>(
+             <TouchableHighlight onPress={()=>this.attatchFile(index)} style={{flexDirection:'row'}}>
+               <Text>첨부파일 {index+1} 추가</Text>
+             </TouchableHighlight>
+            ))
+          }
           </View>
           <View style={{flexDirection: 'row'}}>
             <Text>경력 카테고리코드 </Text>
@@ -280,6 +407,11 @@ class AltApplyFormScreen extends React.Component {
             />
           </View>
           <View>
+            <Button onPress={()=>console.log(
+              '\nacv_file1 : '+JSON.stringify(acv_file1),'\nacv_open : '+JSON.stringify(acv_open),'\nacv_type : '+JSON.stringify(acv_type),'\nacv_year : '+JSON.stringify(acv_year),'\nacv_content : '+JSON.stringify(acv_content),'\nacv_final : '+JSON.stringify( acv_final)
+            )}>
+              states chk
+            </Button>
             <Button style={{margin: 10}}>취소</Button>
             {/* <Button style={{margin:10}} onPress={()=>navigation.navigate('AltApplyComplete')}>완료</Button> */}
             <Button style={{margin: 10}} onPress={() => this.setAltruist()}>
