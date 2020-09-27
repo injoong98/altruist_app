72ee227ccbdb28ff58a556188a683562583dd91c
Tune <cfdw011@gmail.com>
2020-09-27 20:06:10 +0900

    #805 이타주의자 지원시 동의 폼

--- react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/Apply.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3Stack/Apply.js
@@ -1,10 +1,10 @@
 import React,{useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {SafeAreaView, View, TextInput, StyleSheet, ScrollView} from 'react-native'
+import {SafeAreaView, View, TextInput, StyleSheet, ScrollView,TouchableHighlight} from 'react-native'
 import {Layout,Text,TopNavigation,Button,Icon, TopNavigationAction, Radio, CheckBox, Card } from '@ui-kitten/components'
 import { FlatList } from 'react-native-gesture-handler';
-
+import {WriteContentToptab} from '../../../components/WriteContentTopBar';
 
 const BackIcon =  (props) =>(
     <Icon {...props} name = "arrow-back"/>
@@ -29,14 +29,8 @@ class AltApplyScreen extends React.Component{
           <CheckBox
             checked={checked}
             onChange={nextChecked =>{setChecked(nextChecked); this.handleClick(i, nextChecked)}}
-            
-            // onChange={()=>this.handleChange()}
             >
-                {this.checked}
-            {`${i}에 동의합니다.${checked}`}
-            {/* {`${i}에 동의합니다.${this.nextChecked}`}
-            {`${i}에 동의합니다.${this.state.isChecked[i]}`} */}
-            </CheckBox>
+        </CheckBox>
         );
       };
 
@@ -59,25 +53,38 @@ class AltApplyScreen extends React.Component{
         const {navigation} =this.props
         return(
             <SafeAreaView style={styles.container}>
-            <TopNavigation 
-            title="이타주의자" alignment="center" accessoryLeft={this.BackAction} /> 
-                <ScrollView style={styles.wrapper}>
-                    <View>
-                        <Text style={styles.title}> 이타주의자 지원하기 </Text>
-                    </View>
-                    <View>
-                        <Card>
-                            {/* <this.CheckContent content = {`하이`} /> */}
-                            <this.CheckAgrees i = {0} />
-                        </Card>
-                        <Card>
-                            <this.CheckAgrees i = {1} />
-                        </Card>
-                        <Card>
-                            <this.CheckAgrees i = {2} />
-                        </Card>
-                        {/* <Button onPress={()=>navigation.navigate('AltApplyForm')}>지원하기</Button> */}
-                        <Button onPress={()=>this.goNextStep()}>지원하기</Button>
+            <WriteContentToptab
+                text='지원하기'
+                gbckfunc={() => {
+                    this.props.navigation.goBack();
+                }}
+                gbckuse={true}
+                style={{backgroundColor:'#f4f4f4'}}
+            />
+            <ScrollView style={styles.wrapper}>
+                    <View style={styles.agrees}>
+                        <View style={styles.checks}>
+                            <View style={styles.checkbox}>
+                                <this.CheckAgrees i = {0} />
+                                <Text category='h3' style={styles.chkText}>일번</Text>
+                            </View>
+                            <View style={styles.checkbox}>
+                                <this.CheckAgrees i = {1} />
+                                <Text category='h3' style={styles.chkText}>이번</Text>
+                            </View>
+                            <View style={styles.checkbox}>
+                                <this.CheckAgrees i = {2} />
+                                <Text category='h3' style={styles.chkText}>삼번</Text>
+                            </View>
+                        </View>
+                        <TouchableHighlight 
+                            style={styles.btn}
+                            onPress={()=>this.goNextStep()}
+                        >
+                            <Text category='h2' style={styles.btnText}>
+                                다음
+                            </Text>
+                        </TouchableHighlight>
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -91,21 +98,38 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     wrapper :{
-        padding: 10,
-        backgroundColor: "#eaeaea"
-      },
-      title: {
-        marginTop: 16,
-        paddingVertical: 8,
-        borderWidth: 1,
-        borderColor: "#20232a",
-        borderRadius: 6,
-        backgroundColor: "#B09BDE",
-        color: "#20232a",
-        textAlign: "center",
-        fontSize: 30,
-        fontWeight: "bold"
-      }
+        backgroundColor: "#f4f4f4"
+    },
+    agrees:{
+        marginHorizontal:'5%',
+    },
+    checks:{
+        paddingHorizontal:22,
+        paddingVertical:30,
+        borderRadius:17,
+        backgroundColor:'#ffffff'
+    },
+    checkbox:{
+        flexDirection:'row',
+        marginVertical:6     
+    },
+    chkText:{
+        color:'#63579D',
+        fontSize:14,
+        marginLeft:14
+
+    },
+    btn:{
+        backgroundColor:'#63579D',
+        alignItems:'center',
+        justifyContent:'center',
+        paddingVertical:8,
+        borderRadius:8,
+        marginTop:23
+    },
+    btnText:{
+        color:'#ffffff'
+    }
 });
 
 
