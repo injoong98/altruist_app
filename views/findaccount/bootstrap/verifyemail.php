e360b147f55775d69a9ec88afda4d71b3f569755
pickyham <hammieprogrammer@gmail.com>
2020-10-04 17:47:48 +0900

    #962 웹) 이메일 인증 화면 수정 인증화면 웹사이트로 넘어가는 부분 날리기

--- views/findaccount/bootstrap/verifyemail.php
+++ views/findaccount/bootstrap/verifyemail.php
@@ -1,12 +1,20 @@
 <?php $this->managelayout->add_css(element('view_skin_url', $layout) . '/css/style.css'); ?>
-
+<style>
+	footer,
+	nav,
+	header,
+	.sidebar {
+		display: none !important;
+	}
+</style>
 <div class="final col-md-6 col-md-offset-3">
 	<div class="panel panel-default">
 		<div class="panel-heading">이메일 인증</div>
 		<div class="panel-body">
 			<?php echo element('message', $view); ?>
 			<p class="btn_final">
-				<a href="<?php echo site_url(); ?>" class="btn btn-danger btn-sm" title="<?php echo html_escape($this->cbconfig->item('site_title'));?>">홈페이지로 이동</a>
+				<!-- <a href="<?php echo site_url(); ?>" class="btn btn-danger btn-sm" title="<?php echo html_escape($this->cbconfig->item('site_title')); ?>">홈페이지로 이동</a> -->
+				<button onClick="window.close();" class="btn btn-danger btn-sm">창 닫기</button>
 			</p>
 		</div>
 	</div>
