6413d6879a7703b565caed88e5f8b0f9c220d60b
pickyham <hammieprogrammer@gmail.com>
2020-10-08 19:12:04 +0900

    #1061 비밀번호 찾기 기능 수정
    -  탭 별로 기능 수정

--- react_native/AltruistApp/L1Stack/FindPw.js
+++ react_native/AltruistApp/L1Stack/FindPw.js
@@ -21,7 +21,7 @@ const BackIcon = (props) => (
   />
 );
 
-const TitleContent = ({title = 0}) => {
+const TitleContent = ({title}) => {
   return (
     <View style={{paddingBottom: 30}}>
       <Text
@@ -49,8 +49,12 @@ class FindPwScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // findtype : //아이디로...
+      formName: ['idpw_id', 'idpw_hp', 'idpw_email'],
+      formType: ['findbyid', 'findbyhp', 'findidpw'],
       title: ['아이디', '휴대폰번호', '이메일'],
       indexClick: 0,
+      findtype: 'findbyid',
     };
   }
 
@@ -65,15 +69,35 @@ class FindPwScreen extends Component {
 
   SubmitForm = async () => {
     let formdata = new FormData();
-    const {mem_email} = this.state;
+    const {idpw_hp, idpw_id, findtype, idpw_email} = this.state;
 
-    formdata.append('findtype', 'findidpw');
-    formdata.append('idpw_email', mem_email);
+    console.log(this.state);
+    formdata.append('findtype', findtype);
+
+    if (findtype == 'findidpw') {
+      formdata.append('idpw_email', idpw_email);
+    } else if (findtype == 'findbyhp') {
+      formdata.append('idpw_hp', idpw_hp);
+    } else if (findtype == 'findbyid') {
+      formdata.append('idpw_id', idpw_id);
+    } else {
+      return Alert.alert(
+        '접근 실패',
+        '{`올바른 접근 방법이 아닙니다. \n 다시 시도해주세요.`}',
+        [
+          {
+            text: 'OK',
+          },
+        ],
+        {cancelable: false},
+      );
+    }
     console.info('form', this.state);
 
     await axios
       .post(`http://dev.unyict.org/api/findaccount/findpw`, formdata)
       .then((res) => {
+        console.log(res);
         if (res.data.status == 500) {
           //실패 모달
           console.log('실패');
@@ -127,13 +151,14 @@ class FindPwScreen extends Component {
 
   FindPwTab = () => {
     const [selectedIndex, setSelectedIndex] = React.useState(0);
-    console.log(selectedIndex);
+    const {formType} = this.state;
     return (
       <TabBar
         selectedIndex={selectedIndex}
         onSelect={(index) => {
           setSelectedIndex(index);
           this.setState({indexClick: index});
+          this.setState({findtype: formType[selectedIndex]});
         }}>
         <Tab title="아이디" />
         <Tab title="휴대전화" />
@@ -141,11 +166,71 @@ class FindPwScreen extends Component {
       </TabBar>
     );
   };
+
+  toggleOnChangeHandler = (e) => {
+    console.log(e);
+    console.log(e.type);
+    // Deriving the filter that a checkbox is associated too, and getting its value on change
+    // const property = e.target.label;
+    // const val = e.target.value;
+    // this.setState(
+    //   (prevState) => {
+    //     // Taking a copy of the initial filters obj
+    //     const {filters} = prevState;
+    //     // Updating it's property as per the key, value pair retrieved (key being the filter, value being "on" or "off")
+    //     filters[property] = val;
+    //     // Returning the updated object
+    //     return {filters};
+    //   },
+    //   // Since setState is async, all operations that require the updated state, should be done here
+    //   () => {
+    //     console.log(this.state);
+    //   },
+    // );
+  };
+
+  inputChangeHandler = (inputName, inputValue) => {
+    this.setState((state) => ({
+      [inputName]: inputValue, // <-- Put square brackets
+    }));
+  };
+
+  FindPwInput = () => {
+    const {indexClick, title, formName} = this.state;
+    console.log('formName[indexClick]', formName[indexClick]);
+    let formname = [...formName];
+    console.log(formname);
+    return (
+      <Input
+        label={formName[indexClick]}
+        style={styles.inputs}
+        placeholder={`* ${title[indexClick]}`}
+        onChangeText={(text) =>
+          this.inputChangeHandler(formName[indexClick], text)
+        }
+
+        // onChangeText={this.toggleOnChangeHandler}
+        // onEndEditing={() => {
+        //   this.checkEmail(this.state.mem_email);
+        // }}
+        // caption={() =>
+        //   this.state.checkEmailCaption ? (
+        //     <Text category="s2" style={{color: '#ACACAC', paddingLeft: 10}}>
+        //       {this.state.checkEmailCaption}
+        //     </Text>
+        //   ) : (
+        //     <Text> </Text>
+        //   )
+        // }
+      />
+    );
+  };
+
   render() {
     const {title, indexClick} = this.state;
     // const title = this.state;
-    // console.log(this.state);
-    console.log('title -  ', title);
+    console.log(this.state);
+    // console.log('title -  ', title);
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: '#FFFFFF'}}>
         <TopNavigation
@@ -157,27 +242,7 @@ class FindPwScreen extends Component {
         <this.FindPwTab />
         <View style={{flex: 1, justifyContent: 'center'}}>
           <TitleContent title={title[indexClick]} />
-          <Input
-            style={styles.inputs}
-            textContentType="emailAddress" //ios
-            placeholder="* 이메일"
-            onChangeText={(mem_email) => {
-              this.setState({mem_email});
-            }}
-            onEndEditing={() => {
-              // this.checkNotNull();
-              this.checkEmail(this.state.mem_email);
-            }}
-            caption={() =>
-              this.state.checkEmailCaption ? (
-                <Text category="s2" style={{color: '#ACACAC', paddingLeft: 10}}>
-                  {this.state.checkEmailCaption}
-                </Text>
-              ) : (
-                <Text> </Text>
-              )
-            }
-          />
+          <this.FindPwInput titile={title[indexClick]} />
           <Button
             style={{
               alignSelf: 'center',
@@ -189,8 +254,7 @@ class FindPwScreen extends Component {
               !this.state.checkEmailCaption
                 ? this.SubmitForm()
                 : this.setState({
-                    checkEmailCaption:
-                      '입력한 이메일을 다시 한번 확인해주세요.',
+                    checkEmailCaption: '입력한 정보를 다시 한번 확인해주세요.',
                   })
             }>
             PW 재설정
