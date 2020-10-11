e8236677dbf75955b92aff389ac039b002afc587
geun9716 <crintop@gmail.com>
2020-10-11 23:26:26 +0900

    프로필 수정 alert 지우고 뒤로 돌아감

--- react_native/AltruistApp/L1Stack/L2Bottom/L3MyStack/Mypage.js
+++ react_native/AltruistApp/L1Stack/L2Bottom/L3MyStack/Mypage.js
@@ -26,8 +26,8 @@ class Mypage extends React.Component{
 
     static contextType = Signing;
     
-    loadMemInfo = () => {
-      axios.get('http://dev.unyict.org/api/mypage')
+    loadMemInfo = async() => {
+      await axios.get('http://dev.unyict.org/api/mypage')
       .then(res=>{
         this.setState({mem_info:res.data.myinfo,isLoading:false})
         
@@ -155,7 +155,7 @@ class Mypage extends React.Component{
                       <Text category='h2' style={styles.menuTitle}>계정</Text>
                     </View>
                     <View>
-                      <TouchableOpacity style={styles.menuContainer} onPress={()=>navigate('MyProfEdit',{mem_info:this.state.mem_info,onGoback:this.onRefresh})} >
+                      <TouchableOpacity style={styles.menuContainer} onPress={()=>{navigate('MyProfEdit', { mem_info:this.state.mem_info, onGoback: () => this.onRefresh() })}}>
                         <Text style={styles.menuItem}>프로필 수정</Text>
                       </TouchableOpacity>
                       <TouchableOpacity style={styles.menuContainer} onPress={()=>{this.setState({logOutModalVisible:true})}} >
