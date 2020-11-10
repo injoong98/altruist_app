b13767ae84a7f53a46db5d7d82d09ed73ddf13ac
pickyham <hammieprogrammer@gmail.com>
2020-11-10 11:18:06 +0900

    #1148, #1166 회원가입, 비밀번호 변경 화면 수정
    - 디자인 수정

--- views/findaccount/bootstrap/verifyemail.php
+++ views/findaccount/bootstrap/verifyemail.php
@@ -1,21 +1,29 @@
-<?php $this->managelayout->add_css(element('view_skin_url', $layout) . '/css/style.css'); ?>
 <style>
+	@font-face {
+		font-family: ng;
+		src: url(assets\fonts\Jalnan.ttf);
+		/* src: local(※), url(NanumGothic.woff) format('ttf') */
+	}
+
 	footer,
 	nav,
 	header,
-	.sidebar {
+	.sidebar,
+	.header_line {
 		display: none !important;
 	}
 </style>
-<div class="final col-md-6 col-md-offset-3">
+<div style="font-family:none; text-align:center; font-size:2em">
+	<?php echo element('message', $view); ?>
+</div>
+<!-- <div class="final col-md-12 col-md-offset-3">
 	<div class="panel panel-default">
-		<div class="panel-heading">이메일 인증</div>
+		<div style="font-family:'잘난', 'Jalnan', ng; text-align:center; margin : auto 0; font-size: 3em;">이메일 인증</div>
+		<div class="panel-heading" style="font-family:'잘난', 'Jalnan', ng; text-align:center">이메일 인증</div>
 		<div class="panel-body">
-			<?php echo element('message', $view); ?>
-			<p class="btn_final">
-				<!-- <a href="<?php echo site_url(); ?>" class="btn btn-danger btn-sm" title="<?php echo html_escape($this->cbconfig->item('site_title')); ?>">홈페이지로 이동</a> -->
-				<button onClick="window.close();" class="btn btn-danger btn-sm">창 닫기</button>
+			<p style="margin: auto 0 ">
+				
 			</p>
 		</div>
 	</div>
-</div>
+</div> -->
